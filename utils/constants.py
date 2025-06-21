a9081726f8884bb69d7a9d0257f1fdc9612f564e
shuchenliu <shuchenliu2014@gmail.com>
2025-06-20 19:39:53 -0700

    add output writing

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,2 @@
 NODE_INDEX="rtx_kg2_nodes"
+TEMP_DIR="temp_output"
---
+++ utils/writes.py
@@ -0,0 +1,17 @@
+import glob
+
+from utils.constants import TEMP_DIR
+
+
+def write_to_temp(batch_id: int, edges: list[str]):
+    """
+    Write serialized edges to a temporary file.
+    """
+    with open(f"{TEMP_DIR}/{batch_id}.tmp.jsonl", "w") as f:
+        f.write("\n".join(edges) + '\n')
+
+def stitch_temps():
+    with open('merged_edges.jsonl', 'wb') as output:
+        for filepath in sorted(glob.glob(f"{TEMP_DIR}/*.tmp.jsonl")):
+            with open(filepath, "rb") as infile:
+                output.write(infile.read())
