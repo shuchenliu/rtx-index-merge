638c0f6f82834cbd847e28c3bd3fe54ba656dc9f
shuchenliu <shuchenliu2014@gmail.com>
2025-06-23 13:43:55 -0700

    extract edge processing method, preparing for Dask mapping

--- merge_index.py
+++ merge_index.py
@@ -6,7 +6,7 @@ from typing import Optional
 
 from elasticsearch import Elasticsearch
 
-from utils.edges import load_edges
+from utils.edges import load_edges, process_edges
 from utils.make_offsets import get_offsets
 from utils.nodes import get_nodes_details
 from utils.writes import write_to_temp, stitch_temps
@@ -21,52 +21,18 @@ from utils.writes import write_to_temp, stitch_temps
 SERVER = "localhost"
 PORT = 9200
 ES_URL = "http://%s:%d" % (SERVER, PORT)
-
-
 EDGE_FILE = "edges_sample.jsonl"
 
-
-def process_edges(start: int, end: Optional[int]) -> list[str]:
-    loaded = load_edges(EDGE_FILE, start, end)
-    # todo use worker to get client
-    client = Elasticsearch(ES_URL)
-
-
-    # 0. get `subject` and `object`
-    def ids_getter(id_set: set, edge: dict):
-        if "subject" in edge:
-            id_set.add(edge["subject"])
-        if "object" in edge:
-            id_set.add(edge["object"])
-
-        return id_set
-
-    ids = reduce(ids_getter, loaded, set()) # functional programming
-
-
-    # 1. use es to get details
-    details = get_nodes_details(client, list(ids))
-
-    # 2. update edges and write back to file
-    for index, edge in enumerate(loaded):
-        if "subject" in edge:
-            edge["subject"] = details[edge["subject"]]
-        if "object" in edge:
-            edge["object"] = details[edge["object"]]
-
-        loaded[index] = json.dumps(edge)
-
-
-    return loaded
 
 
 
 def main():
     os.makedirs("temp_output", exist_ok=True)
     offsets = get_offsets(EDGE_FILE)
+    es_client = Elasticsearch(ES_URL)
 
     for start_index, start in enumerate(offsets):
-        updated_edges = process_edges(start, offsets[start_index + 1] if start_index + 1 < len(offsets) else None)
+        updated_edges = process_edges(es_client, EDGE_FILE, start, offsets[start_index + 1] if start_index + 1 < len(offsets) else None)
         write_to_temp(start_index, updated_edges)
 
     # write final output file
--- utils/edges.py
+++ utils/edges.py
@@ -1,5 +1,9 @@
 import json
+from functools import reduce
 from typing import Optional
+
+from elasticsearch import Elasticsearch
+from utils.nodes import get_nodes_details
 
 
 def load_edges(target_file:str, start: int, end:Optional[int]) -> list:
@@ -22,3 +26,33 @@ def load_edges(target_file:str, start: int, end:Optional[int]) -> list:
         loaded = list(map(json.loads, lines))
 
         return loaded
+
+
+def process_edges(es_client: Elasticsearch, target_file:str, start: int, end: Optional[int]) -> list[str]:
+    loaded = load_edges(target_file, start, end)
+    # 0. get `subject` and `object`
+    def ids_getter(id_set: set, edge: dict):
+        if "subject" in edge:
+            id_set.add(edge["subject"])
+        if "object" in edge:
+            id_set.add(edge["object"])
+
+        return id_set
+
+    ids = reduce(ids_getter, loaded, set()) # functional programming
+
+
+    # 1. use es to get details
+    details = get_nodes_details(es_client, list(ids))
+
+    # 2. update edges and write back to file
+    for index, edge in enumerate(loaded):
+        if "subject" in edge:
+            edge["subject"] = details[edge["subject"]]
+        if "object" in edge:
+            edge["object"] = details[edge["object"]]
+
+        loaded[index] = json.dumps(edge)
+
+
+    return loaded
